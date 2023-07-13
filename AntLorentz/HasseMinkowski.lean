7bf8b416c014df241acf24adefc45ebed4298df5
Alex J. Best <alex.j.best@gmail.com>
2023-07-13 13:58:50 +0100

    stuff

--- AntLorentz/HasseMinkowski.lean
+++ AntLorentz/HasseMinkowski.lean
@@ -3,13 +3,14 @@ import Mathlib.LinearAlgebra.QuadraticForm.Basic -- quadratic forms
 import Mathlib.LinearAlgebra.TensorProduct -- tensor products (for base change)
 import Mathlib.LinearAlgebra.Dimension -- rank of modules
 import Mathlib.NumberTheory.Padics.PadicNumbers
-import AntLorentz.BaseChange
-
-
-variable [CommSemiring R] [AddCommMonoid M] [Module R M] [CommSemiring A] [Algebra R A]
+import AntLorentz.Diagonalize
 
 namespace QuadraticForm
+
+variable [Semiring R] [AddCommMonoid M] [Module R M]
+
 abbrev Isotropic (Q : QuadraticForm R M) : Prop := ¬ Anisotropic (Q)
+
 end QuadraticForm
 
 /-!
@@ -40,17 +41,45 @@ Thoughts:
 - https://etd.ohiolink.edu/apexprod/rws_etd/send_file/send?accession=osu1338317481&disposition=inline seems an ok reference for 2,3
 -/
 
+section base_change
+
+/-
+
+## Base extension of quadratic forms
+
+Unfortunately we don't seem to have this in the library, so we have
+to develop it ourselves including making all the basic results which we'll need.
+Note that we also make the theory in maximal generality (for example
+we use semirings instead of rings, so the theory works for quadratic
+forms over the naturals)
+
+-/
+
+-- Let `M` be an `R`-module
+variable (R : Type) (M : Type) [CommRing R] [AddCommGroup M] [Module R M]
+
+-- Let `A` be an `R`-algebra
+variable (A : Type) [Semiring A] [Algebra R A]
 
 open TensorProduct -- this line gives us access to ⊗ notation
 
 -- Let's be lazy and assume 1/2 ∈ R
 variable [Invertible (2 : R)]
 
+def BilinForm.baseChange (F : BilinForm R M) : BilinForm A (A ⊗[R] M) :=
+  --let L := BilinForm.toLinHom
+  sorry
 
--- temporary NOT CORRECT YET
-lemma base_change_module_rank_preserved : Module.rank R M = Module.rank A (A ⊗[R] M) :=by sorry
+/-- If `F : QuadraticForm R M` and `A` is an `R`-algebra then `F.baseChange A`
+is the associated quadratic form on `M ⊗[R] A` -/
+def QuadraticForm.baseChange (F : QuadraticForm R M) : QuadraticForm A (A ⊗[R] M) := by
+  let B : BilinForm R M := associatedHom R F
+  let B' : BilinForm A (A ⊗[R] M) := B.baseChange R M A -- base change
+  exact B'.toQuadraticForm
 
 
+lemma QuadraticForm.baseChange.Equivalent (Q S : QuadraticForm R M) (h : Q.Equivalent S) :
+    (baseChange R M A Q).Equivalent (baseChange R M A S) := sorry
 
 -- Let V be a ℚ-vector space
 variable {V : Type} [AddCommGroup V] [Module ℚ V]
@@ -64,53 +93,33 @@ variable (F : QuadraticForm ℚ V)
 /-- A quadratic form over ℚ is everywhere locally isotropic if it has nontrivial
 p-adic points for all p, and real points. -/
 def QuadraticForm.EverywhereLocallyIsotropic :=
-  (∀ (p : ℕ) [Fact (p.Prime)], (F.baseChange ℚ_[p]).Isotropic) ∧
-  (F.baseChange ℝ).Isotropic
+  (∀ (p : ℕ) [Fact (p.Prime)], (F.baseChange ℚ V ℚ_[p]).Isotropic) ∧
+  (F.baseChange ℚ V ℝ).Isotropic
 
 /-- The *statement* of the Hasse-Minkowski theorem. -/
-def Hasse_Minkowski (F : QuadraticForm ℚ V) : Prop :=
+def QuadraticForm.Hasse_Minkowski (F : QuadraticForm ℚ V) : Prop :=
   F.Isotropic ↔ F.EverywhereLocallyIsotropic
 
+open QuadraticForm
+
 -- a nontrivial project (probably publishable if someone does it)
-theorem Hasse_Minkowski_proof : ∀ (F : QuadraticForm ℚ V), Hasse_Minkowski F := sorry
+theorem Hasse_Minkowski_proof : ∀ (F : QuadraticForm ℚ V), F.Hasse_Minkowski := sorry
 
 -- some easier problems
 
 -- (0) dim(V)=0 case
 
-variable (k W : Type) [Field k] [AddCommGroup W]
+variable (k W : Type) [Field k] [AddCommMonoid W]
 
-lemma anisotropic_of_quadform_dim_zero [Module k W] (Q : QuadraticForm k W) (h : Module.rank k W = 0) : Q.Anisotropic := by
-   intro (w : W)
-   intro 
-   rw [rank_zero_iff_forall_zero] at h
-   exact h w
-   #check anisotropic_of_quadform_dim_zero
+theorem quadform_zero_dim_eq_zero [Module k W] (Q : QuadraticForm k W) (h : Module.rank W = 0) : Q = 0 := by sorry
 
-theorem Hasse_Minkowski0 (hV : Module.rank ℚ V = 0) : ∀ (F : QuadraticForm ℚ V), Hasse_Minkowski F := by
-   intro F
-   rw [Hasse_Minkowski]
-   constructor 
-   · contrapose
-     intro 
-     rw [QuadraticForm.Isotropic]
-     simp
-     apply anisotropic_of_quadform_dim_zero _ _ F hV
-   · contrapose
-     intro 
-     rw [QuadraticForm.EverywhereLocallyIsotropic]
-     push_neg
-     intro 
-     simp 
-     apply anisotropic_of_quadform_dim_zero
-     rw [← base_change_module_rank_preserved, hV] 
+theorem Hasse_Minkowski0 (hV : Module.rank V = 0) :
+    ∀ (F : QuadraticForm ℚ V), Hasse_Minkowski F := by
+    sorry
+    -- idea: no non-zero elements of V:
+    -- rank_zero_iff_forall_zero -- says that every element in a rank 0 module is 0
+--------------------------------------------------------------------
 
-lemma zero_of_quadform_dim_zero [Module k W] (Q : QuadraticForm k W) (h : Module.rank W = 0) : Q = 0 := by sorry
-  -- rank_zero_iff_forall_zero 
-
-
-   
-    
 
 
 
@@ -120,6 +129,19 @@ lemma zero_of_quadform_dim_zero [Module k W] (Q : QuadraticForm k W) (h : Module
 theorem Hasse_Minkowski1 (hV : Module.rank V = 1) :
     ∀ (F : QuadraticForm ℚ V), Hasse_Minkowski F := sorry
 
+
+
+/--
+⊗
+
+
+-/
+lemma HM_of_Equivalent {Q S : QuadraticForm ℚ V} (h : Q.Equivalent S) :
+    Q.Hasse_Minkowski ↔ S.Hasse_Minkowski := by
+  simp only [Hasse_Minkowski, Isotropic, EverywhereLocallyIsotropic] at *
+  simp [anisotropic_iff _ _ h]
+  rw [anisotropic_iff _ _ (baseChange.Equivalent ℚ V ℝ _ _ h)]
+  simp_rw [anisotropic_iff _ _ (baseChange.Equivalent ℚ V ℚ_[_] _ _ h)]
 
 
 -- (2) dim(V)=2 case
