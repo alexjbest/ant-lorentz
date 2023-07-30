ef7782418b18e9b404cad75c347dcc003dd97927
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-07-30 21:47:17 +0100

    add a couple of sorrys

--- AntLorentz/CyclotomicCharacter.lean
+++ AntLorentz/CyclotomicCharacter.lean
@@ -14,26 +14,31 @@ import Mathlib.RingTheory.RootsOfUnity.Basic
 Let `L` is a field and let `n : ℕ+` be a positive integer. If `μₙ` is the
 group of `n`th roots of unity in `L` then any field automorphism `g` of `L`
 induces an automorphism of `μₙ` which, being a cyclic group, must be of
-the form `ζ ↦ ζ^j` for some integer `j = j(g)`, well-defined in `ZMod d` with
-`d` the cardinality of `μₙ`. The function `j` is a group homomorphism 
-`(L ≃+* L) →* ZMod d`. 
+the form `ζ ↦ ζ^j` for some integer `j = j(g)`, well-defined in `ZMod d`, with
+`d` the cardinality of `μₙ`. The function `j` is a group homomorphism
+`(L ≃+* L) →* ZMod d`.
 
-If `L` is separably closed (e.g. algebraically closed) and `p` is a prime
+Future work: If `L` is separably closed (e.g. algebraically closed) and `p` is a prime
 number such that `p ≠ 0` in `L`, then applying the above construction with
 `n = p^i` (noting that the size of `μₙ` is `p^i`) gives a compatible collection of
 group homomorphisms `(L ≃+* L) →* ZMod (p^i)` which glue to give
-a group homomorphism `(L ≃+* L) →* ℤₚ`; this is the `p`-adic cyclotomic character. 
+a group homomorphism `(L ≃+* L) →* ℤₚ`; this is the `p`-adic cyclotomic character.
 
 ## Important definitions
 
 Let `L` be a field, `g : L ≃+* L` and `n : ℕ+`. Let `d` be the number of `n`th roots of `1` in `L`.
 
-* `ModularCyclotomicCharacter n : L ≃+* L →* ZMod d` sends `g` to the unique `j` such that `g(ζ)=ζ^j` for
-    all `ζ : rootsOfUnity n L`.
+* `ModularCyclotomicCharacter n : L ≃+* L →* ZMod d` sends `g` to the unique `j` such
+   that `g(ζ)=ζ^j` for all `ζ : rootsOfUnity n L`.
 
 ## Todo
 
-Glue the `ModularCyclotomicCharacter`s together. Define the cyclotomic character.
+* Prove the compatibility of `ModularCyclotomicCharacter n` and `ModularCyclotomicCharacter m`
+if `n ∣ m`.
+
+* Define the cyclotomic character.
+
+* Prove that it's continuous.
 
 ## Tags
 
@@ -57,7 +62,7 @@ theorem rootsOfUnity.integer_power_of_ringEquiv (g : L ≃+* L) :
   exact ⟨m, by simp [← hm]⟩
 
 /-- `ModularCyclotomicCharacter_aux g n` is a non-canonical auxiliary integer `j`,
-   only well-defined modulo the number of n'th roots of unity in `L`, such that `g(ζ)=ζ^j` 
+   only well-defined modulo the number of `n`'th roots of unity in `L`, such that `g(ζ)=ζ^j`
    for all `n`'th roots of unity `ζ` in `L`. -/
 noncomputable def ModularCyclotomicCharacter_aux (g : L ≃+* L) (n : ℕ+) : ℤ :=
   (rootsOfUnity.integer_power_of_ringEquiv n g).choose
@@ -70,11 +75,11 @@ theorem ModularCyclotomicCharacter_aux_spec (g : L ≃+* L) (n : ℕ+) :
 /-- If `g` is a field automorphism of `L`, and `n : ℕ+`, then
   `ModularCyclotomicCharacter.toFun n g` is the `j : ZMod d` such that `g(ζ)=ζ^j` for all
   `n`'th roots of unity. Here `d` is the number of `n`th roots of unity in `L`. -/
-noncomputable def ModularCyclotomicCharacter.toFun (n : ℕ+) (g : L ≃+* L) : 
+noncomputable def ModularCyclotomicCharacter.toFun (n : ℕ+) (g : L ≃+* L) :
     ZMod (Fintype.card (rootsOfUnity n L)) :=
   ModularCyclotomicCharacter_aux g n
 
--- This appears to be missing from the library
+-- This appears to be missing from the library. It should not be in this file.
 theorem Group.pow_eq_zpow_mod {G : Type _} [Group G] {x : G} (m : ℤ) {n : ℕ} (h : x ^ n = 1) :
     x ^ m = x ^ (m % (n : ℤ)) := by
   have h2 : x ^ m = x ^ ((n : ℤ) * (m / (n : ℤ)) + m % (n : ℤ)) :=
@@ -89,7 +94,7 @@ local notation "χ" => ModularCyclotomicCharacter.toFun
 theorem spec (g : L ≃+* L) (n : ℕ+) :
     ∀ t : rootsOfUnity n L, g (t : Lˣ) = (t ^ (χ n g).val : Lˣ) := by
   rintro t
-  rw [ModularCyclotomicCharacter_aux_spec g n t, ← zpow_ofNat, ModularCyclotomicCharacter.toFun, 
+  rw [ModularCyclotomicCharacter_aux_spec g n t, ← zpow_ofNat, ModularCyclotomicCharacter.toFun,
       ZMod.val_int_cast, ←Group.pow_eq_zpow_mod _ pow_card_eq_one]
 
 lemma ext {G : Type _} [Group G] [Fintype G] [IsCyclic G]
@@ -102,9 +107,9 @@ lemma ext {G : Type _} [Group G] [Fintype G] [IsCyclic G]
   subst hGcard
   simpa [ZMod.nat_cast_val, ZMod.cast_id'] using h
 
-example (n : ℕ) [NeZero n] : 0 < n := by exact Fin.size_positive'
-lemma subsingleton_of_card_eq_one {X : Type _} [Fintype X] (h : Fintype.card X = 1) : 
-    Subsingleton X := by 
+-- This should not be in this file.
+lemma subsingleton_of_card_eq_one {X : Type _} [Fintype X] (h : Fintype.card X = 1) :
+    Subsingleton X := by
   rw [Fintype.card_eq_one_iff_nonempty_unique] at h
   cases h
   infer_instance
@@ -122,7 +127,7 @@ lemma id : χ n (RingEquiv.refl L) = 1 := by
     obtain rfl : ζ = 1 := by apply Subsingleton.elim
     simp
 
-lemma comp (g h : L ≃+* L) : χ n (g * h) = 
+lemma comp (g h : L ≃+* L) : χ n (g * h) =
     χ n g * χ n h := by
   refine ext (G := rootsOfUnity n L) rfl ?_
   intro ζ
@@ -131,8 +136,8 @@ lemma comp (g h : L ≃+* L) : χ n (g * h) =
   change g (h (ζ : Lˣ)) = _
   rw [spec, spec, mul_comm, ← pow_mul, eq_comm]
   congr 2
-  simp only [pow_eq_pow_iff_modEq, ← ZMod.nat_cast_eq_nat_cast_iff, SubmonoidClass.coe_pow, ZMod.nat_cast_val, Nat.cast_mul]
-  apply ZMod.cast_mul (m := orderOf ζ) orderOf_dvd_card_univ
+  simp only [pow_eq_pow_iff_modEq, ← ZMod.nat_cast_eq_nat_cast_iff, SubmonoidClass.coe_pow,
+    ZMod.nat_cast_val, Nat.cast_mul, ZMod.cast_mul (m := orderOf ζ) orderOf_dvd_card_univ]
 
 end ModularCyclotomicCharacter
 
@@ -142,8 +147,19 @@ where `d` is the number of `n`'th roots of unity in `L`. It is uniquely
 characterised by the property that `g(ζ)=ζ^(ModularCyclotomicCharacter n g)`
 for `g` an automorphism of `L` and `ζ` an `n`th root of unity. -/
 noncomputable
-def ModularCyclotomicCharacter (n : ℕ+) : 
+def ModularCyclotomicCharacter (n : ℕ+) :
     (L ≃+* L) →* ZMod (Fintype.card { x // x ∈ rootsOfUnity n L }) where
       toFun := ModularCyclotomicCharacter.toFun n
       map_one' := ModularCyclotomicCharacter.id n
       map_mul' := ModularCyclotomicCharacter.comp n
+
+variable (L) 
+
+lemma _root_.rootsOfUnity.card_dvd_of_dvd {a b : ℕ+} (hab : a ∣ b) :
+    Fintype.card (rootsOfUnity a L) ∣ Fintype.card (rootsOfUnity b L) := sorry
+
+variable {L}
+
+lemma foobar (a b : ℕ+) (hab : a ∣ b) (g : L ≃+* L) :
+    ZMod.castHom (rootsOfUnity.card_dvd_of_dvd L hab) (ZMod (Fintype.card (rootsOfUnity a L))) 
+    (ModularCyclotomicCharacter b g) = ModularCyclotomicCharacter a g := sorry
